461b009d71b23272bff2a79d2a6b9a6780634a21
LuchLuch <31387696+LuchLuch@users.noreply.github.com>
2017-08-31 21:56:06 +0300

    Update Saper.py

--- Saper.py
+++ Saper.py
@@ -1,6 +1,6 @@
 import tkinter
 root = tkinter.Tk()
 root.title("Saper")
-canvas = tkinter.Canvas(root, width=280, height=280, bg="white")
+canvas = tkinter.Canvas(root, width=270, height=270, bg="white")
 canvas.pack()
 root.mainloop()
